790848b8b1fccc697f1cfea2eb86a8f542cacd50
Imed <imed.benamor.pro@gmail.com>
2026-03-26 10:34:58 +0100

    manipulation data

--- scriptt.py
+++ scriptt.py
@@ -18,36 +18,10 @@ avg_platform = df.groupby('platform')['score'].mean()
 print("\nScore moyen par plateforme :")
 print(avg_platform)
 
+# KPI : Key Performance Indicator
+# En fr : Indicateur Clé de Performance
 # 5. Calcul KPI : ratio skill
 df['skill_ratio'] = df['score'] / (df['deaths'] + 1)
 
 print("\nDataset avec skill_ratio :")
 print(df)
-
-
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-# --- Matplotlib (Statique) ---
-plt.figure(figsize=(8, 4))
-plt.bar(df['platform'].value_counts().index, df['platform'].value_counts().values, color='skyblue')
-plt.title("Répartition des joueurs par plateforme")
-plt.ylabel("Nombre de joueurs")
-plt.show()
-
-# --- Plotly (Interactif) ---
-# Analyse de la corrélation Score vs Temps de jeu
-fig = px.scatter(df, x="session_duration", y="score", 
-                 size="deaths", color="platform",
-                 hover_name="player_id", 
-                 title="Performance des joueurs (Taille = Morts)")
-fig.show()
-
-# --- Heatmap (Activité) ---
-# Simulation d'une grille 24h x 7j
-activity_data = np.random.randint(0, 100, size=(7, 24))
-fig_heat = px.imshow(activity_data, 
-                     labels=dict(x="Heure de la journée", y="Jour de la semaine", color="Joueurs Co"),
-                     x=[f"{i}h" for i in range(24)],
-                     y=["Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"])
-fig_heat.show()
